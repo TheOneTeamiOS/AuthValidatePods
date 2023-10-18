357b8274cea5679dcfd6e27fa718ab538085d708
sapanaben.chettipalli <sapana.chettipalli@theonetechnologies.co.in>
2023-10-18 16:37:55 +0530

    update validations

---
+++ AuthValidatePods/Classes/ReplaceMe.swift
@@ -0,0 +1,8 @@
+func isValidEmail(email: String) -> Bool {
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+    let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    print("")
+    return emailPred.evaluate(with: email)
+}
+
