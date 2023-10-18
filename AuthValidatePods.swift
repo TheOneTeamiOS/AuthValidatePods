97aa2c231f2ca90ff4b1b035674a7b0301ed4324
sapanaben.chettipalli <sapana.chettipalli@theonetechnologies.co.in>
2023-10-18 14:00:25 +0530

    All files

---
+++ AuthValidatePods.swift
@@ -0,0 +1,8 @@
+//
+//  AuthValidatePods.swift
+//  AuthValidatePods
+//
+//  Created by theonetech on 18/10/23.
+//
+
+import Foundation
---
+++ Example/Pods/Target Support Files/AuthValidatePods/AuthValidatePods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AuthValidatePods : NSObject
+@end
+@implementation PodsDummy_AuthValidatePods
+@end
