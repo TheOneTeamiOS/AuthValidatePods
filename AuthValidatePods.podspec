244edec139a2102544d1c7cb83d3e75a9d77cf58
sapanaben.chettipalli <sapana.chettipalli@theonetechnologies.co.in>
2023-10-18 17:01:20 +0530

    New File changes

--- AuthValidatePods.podspec
+++ AuthValidatePods.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AuthValidatePods'
-  s.version          = '0.1.0'
+  s.version          = '4.0.0'
   s.summary          = 'A short description of AuthValidatePods.'
 
 # This description is used to generate tags and improve search results.
--- AuthValidatePods/Classes/AuthValidatePods.swift
+++ AuthValidatePods/Classes/AuthValidatePods.swift
@@ -11,6 +11,6 @@ func isValidEmail(email: String) -> Bool {
     let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
     let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-    print("Validations")
+    print("")
     return emailPred.evaluate(with: email)
 }
