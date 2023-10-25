d188e1283b9bb274bebde17e13336f1942906e3a
sapanaben.chettipalli <sapana.chettipalli@theonetechnologies.co.in>
2023-10-25 17:04:21 +0530

    create labels

--- Classes/ValidateVC.swift
+++ Classes/ValidateVC.swift
@@ -11,6 +11,10 @@ public class ValidateVC: UIViewController {
     
     @IBOutlet weak public var label: UILabel?
     
+    @IBOutlet public var myLabel: UILabel!
+    
+    @IBOutlet public var customLabel: CustomLabel!
+    
     public var intValue = 0
     
     public override func viewDidLoad() {
@@ -21,7 +25,33 @@ public class ValidateVC: UIViewController {
         
         label?.text = "hello sapna"
         print(label?.text ?? "")
+        
+        myLabel.text = "Welcome mylabel"
+        print(myLabel.text ?? "")
+        
+        customLabel.text = "Welcome custom label"
+        print(customLabel.text ?? "")
 
     }
     
 }
+
+
+public class CustomLabel: UILabel {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        // Customize the label's properties here
+        textColor = UIColor.black
+        font = UIFont.systemFont(ofSize: 17)
+    }
+}
+
